e0c4da8895c18d6f1a71da5234f4e91c06f91252
JosephSeong <joseph0903@naver.com>
2023-12-01 17:26:17 +0900

    first commit

---
+++ CalculatorLV1.playground/Contents.swift
@@ -0,0 +1,34 @@
+class Calculator {
+    func calculate(operation: String, firstNumber: Int, secondNumber: Int) -> Double {
+        if operation == "+" {
+            return Double(firstNumber + secondNumber)
+        }
+
+        if operation == "-" {
+            return Double(firstNumber - secondNumber)
+        }
+
+        if operation == "*" {
+            return Double(firstNumber * secondNumber)
+        }
+
+        if operation == "/" {
+            return Double(firstNumber) / Double(secondNumber)
+        }
+
+        // 정확한 operation이 전달되지 않을 시 0을 리턴(nil을 리턴해도 됨)
+        return 0
+    }
+}
+
+let calculator = Calculator()
+
+let addResult = calculator.calculate(operation: "+", firstNumber: 5, secondNumber: 3)
+let subtractResult = calculator.calculate(operation: "-", firstNumber: 8, secondNumber: 4)
+let multiplyResult = calculator.calculate(operation: "*", firstNumber: 6, secondNumber: 2)
+let divideResult = calculator.calculate(operation: "/", firstNumber: 10, secondNumber: 2)
+
+print("addResult: \(addResult)")
+print("subtractResult: \(subtractResult)")
+print("multiplyResult: \(multiplyResult)")
+print("divideResult: \(divideResult)")
