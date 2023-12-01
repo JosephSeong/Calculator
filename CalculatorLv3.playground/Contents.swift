5229c898b9863c8dc8abf1974ee773985ceacb90
JosephSeong <joseph0903@naver.com>
2023-12-01 19:01:36 +0900

    add Lv3

---
+++ CalculatorLv3.playground/Contents.swift
@@ -0,0 +1,64 @@
+class AddOperation {
+    func operate(firstNumber: Int, secondNumber: Int) -> Double {
+        return Double(firstNumber + secondNumber)
+    }
+}
+
+class SubtractOperation {
+    func operate(firstNumber: Int, secondNumber: Int) -> Double {
+        return Double(firstNumber - secondNumber)
+    }
+}
+
+class MultiplyOperation {
+    func operate(firstNumber: Int, secondNumber: Int) -> Double {
+        return Double(firstNumber * secondNumber)
+    }
+}
+
+class DivideOperation {
+    func operate(firstNumber: Int, secondNumber: Int) -> Double {
+        return Double(firstNumber / secondNumber)
+    }
+}
+
+class Calculator {
+    func calculate(operation: String, firstNumber: Int, secondNumber: Int) -> Double {
+        if operation == "+" {
+            return Double(firstNumber + secondNumber)
+        }
+
+        if operation == "-" {
+            return Double(firstNumber - secondNumber)
+        }
+
+        if operation == "*" {
+            return Double(firstNumber * secondNumber)
+        }
+
+        if operation == "/" {
+            return Double(firstNumber) / Double(secondNumber)
+        }
+
+        if operation == "%" {
+            return Double(firstNumber % secondNumber)
+        }
+
+        // 정확한 operation이 전달되지 않을 시 0을 리턴(nil을 리턴해도 됨)
+        return 0
+    }
+}
+
+let calculator = Calculator()
+
+let addResult = calculator.calculate(operation: "+", firstNumber: 5, secondNumber: 3)
+let subtractResult = calculator.calculate(operation: "-", firstNumber: 8, secondNumber: 4)
+let multiplyResult = calculator.calculate(operation: "*", firstNumber: 6, secondNumber: 2)
+let divideResult = calculator.calculate(operation: "/", firstNumber: 10, secondNumber: 2)
+let remainderResult = calculator.calculate(operation: "%", firstNumber: 10, secondNumber: 3)
+
+print("addResult: \(addResult)")
+print("subtractResult: \(subtractResult)")
+print("multiplyResult: \(multiplyResult)")
+print("divideResult: \(divideResult)")
+print("remainderResult: \(remainderResult)")
